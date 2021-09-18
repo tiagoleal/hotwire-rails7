ba203533b082501ea81b0b5b47b35968f279626c
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-09-18 15:26:13 -0300

    chore: add controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :tweets
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  resources :products
+  resources :shopping_carts
 end
