a09a93a3a7dadcb37977187bbaa6b13e7430c933
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-09-19 15:19:04 -0300

    feat: products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,59 @@
 class ProductsController < ApplicationController
-  def create
-  end
+  before_action :set_products, only: %i[edit update destroy]
 
   def index
+    @products = Product.order(id: :asc)
   end
 
-  def show
+  def new
+    @product = Product.new
+  end
+
+  def edit; end
+
+  def create
+    @product = Product.new
+    @product.attributes = products_params
+    if save_product!
+      redirect_to products_path	, notice: "#{@product.name} cadastrada com sucesso!"
+    else
+      alert_errors
+    end
   end
 
   def update
+    @product.attributes = products_params
+    if save_product!
+      redirect_to products_path	, notice: "#{@product.name} atualizada com sucesso!"
+    else
+      alert_errors
+    end
   end
 
   def destroy
+    if @product.destroy
+      redirect_to products_path	, notice: "#{@product.name} excluída com sucesso!"
+    else
+      alert_errors
+    end
+  end
+
+  private
+
+  def save_product!
+    @product.save!
+  end
+
+  def alert_errors
+    redirect_to products_path	, alert: @product.errors.full_messages.to_sentence
+  end
+
+  def set_products
+    @product = Product.find(params[:id])
+  end
+
+  def products_params
+    params.require(:product).permit(:description,
+      :price, :category_id, :status)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,4 @@ RSpec.describe User, type: :model do
     user = create(:user)
     expect(user).to be_valid
   end
-
-  context 'Validates' do
-    it { is_expected.to validate_presence_of(:name)}
-  end
 end
