ee234696ea94a579d8f8342e853249d5b1f6a997
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-09-19 14:27:15 -0300

    feat: configurate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cadastrando category...."
+Category.create(description: "Bebidas")
+Category.create(description: "Alimentos")
+
+puts "Cadastrando product...."
+Product.create(category_id: 1, description: "Coca Cola", price: 4.00, status: 1)
+Product.create(category_id: 1, description: "Guaraná", price: 2.99, status: 1)
+Product.create(category_id: 1, description: "Sprite", price: 3.50, status: 1)
+Product.create(category_id: 1, description: "Cerveja Brahma", price: 2.88, status: 1)
+Product.create(category_id: 1, description: "Cerveja Skol", price: 2.67, status: 1)
+
+puts "Cadastrando product...."
+Product.create(category_id: 2, description: "Leite condensando", price: 5.00, status: 1)
+Product.create(category_id: 2, description: "Arroz", price: 15.00, status: 1)
+Product.create(category_id: 2, description: "Feijão", price: 10.00, status: 1)
