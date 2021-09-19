0edb6c6936bc9c3a17a42c9a7acca77ae4ba6550
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-09-19 14:18:55 -0300

    test: models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with name ' do
+    user = create(:user)
+    expect(user).to be_valid
+  end
+
+  context 'Validates' do
+    it { is_expected.to validate_presence_of(:name)}
+  end
 end
