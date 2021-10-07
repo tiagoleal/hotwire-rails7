39e91ada152ccb0dcc08c40a23c4d6b53ccf3f84
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-10-06 23:44:09 -0300

    chore: add order and order product

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,24 @@ puts "Register product...."
 Product.create(category_id: 2, code: "0006", description: "Leite condensando", price: 5.00, status: 1)
 Product.create(category_id: 2, code: "0007", description: "Arroz", price: 15.00, status: 1)
 Product.create(category_id: 2, code: "0008", description: "Feijão", price: 10.00, status: 1)
+
+
+puts "Order...."
+Order.create(
+  status: 'waiting',
+  user_id: 1
+)
+
+OrderProduct.create(
+  order_id: 1,
+  product_id: 1,
+  unit_price: 4.00,
+  quantity: 1
+)
+
+OrderProduct.create(
+  order_id: 1,
+  product_id: 2,
+  unit_price: 2.99,
+  quantity: 1
+)
