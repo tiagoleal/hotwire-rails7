47b2e4928dcd9b024dbee8e0596a3b95960ebaad
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-10-10 09:34:04 -0300

    fix: remove parameters

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,4 @@
 class Order < ApplicationRecord
-  after_create_commit { broadcast_prepend_to :shoppingCartList }
-  after_update_commit { broadcast_replace_to :shoppingCartList }
-  after_destroy_commit { broadcast_remove_to :shoppingCartList }
-
-
   belongs_to :user
 
   has_many :order_products
@@ -16,7 +11,7 @@ class Order < ApplicationRecord
 
   enum status: {'waiting': 1, 'confirm': 2}
 
-  private 
+  private
 
   def subtotal
     order_products.collect { |order_prod| order_prod.valid? ? order_prod.unit_price * order_prod.quantity : 0}.sum
