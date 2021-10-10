8d1edd9c7e8ae672a9de315f36788d77669875b3
Tiago Leal <tiago.sleal12@gmail.com>
2021-10-10 17:47:51 -0300

    chore: refactor shopping carts

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -4,60 +4,36 @@ class ShoppingCartsController < ApplicationController
   before_action :find_by_product, only: %i[search]
 
   def index
-    order_status
-    total
+    order_list
   end
 
   def search
+
     if params[:search_term].present?
       if @product.present?
-        # order_obj = order_status
-
-        binding.pry
-        # order_status
-
-
-
-        unless order_status
-          save_order
-          # save_order_product(order)
-          # order_obj = order
-        end
-
-        unless product_already_exist?
-          save_order_product
-        end
+        save_order unless order_list
+        save_order_product unless order_product_exist?
       end
     end
-
-    total
     respond_to do |format|
-      format.html do
-        redirect_to shopping_carts_path
-      end
-      format.json { render :show, status: :created, location: @orders }
+      redirect_order(format)
     end
   end
 
-  def show
-  end
+  def show; end
 
   def update
     respond_to do |format|
       if @order.update(status: 'confirm')
-        format.html do
-          redirect_to shopping_carts_path
-        end
-        format.json { render :show, status: :created, location: @order }
+        redirect_order(format)
       else
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end  
   end
 
- 
   def destroy
-    @orderProducts.destroy
+    @order_products.destroy
     respond_to do |format|
       format.html { redirect_to shopping_carts_path }
       format.json { head :no_content }
@@ -67,7 +43,7 @@ class ShoppingCartsController < ApplicationController
   private
 
   def set_order_products
-    @orderProducts = OrderProduct.find_by(
+    @order_products = OrderProduct.find_by(
       order_id: params[:shopping_cart_id],
       product_id: params[:product_id]
     )
@@ -75,10 +51,6 @@ class ShoppingCartsController < ApplicationController
 
   def set_orders
     @order = Order.find(params[:id])
-  end
-
-  def attributes_params
-    @order.attributes = products_params
   end
 
   def find_by_product
@@ -89,45 +61,33 @@ class ShoppingCartsController < ApplicationController
     OrderProduct.create(
       order_id: @orders.id,
       product_id: @product.id,
-      unit_price: @product.price,
       quantity: 1
     )
   end
 
   def save_order
-    # @orders = Order.create_order
-
     @orders = Order.new(
       status: :waiting,
       user_id: current_user.id
     )
     @orders.save
-    # @orders = Order.find_by(status: 'waiting')
-
-    # @order = Order.new(
-    #   status: 'waiting',
-    #   user_id: current_user.id
-    # )
-    # @order.save
-    # @orders = Order.find_by(status: 'waiting')
   end
 
-  def total
-    order_status
+  def order_list
+    @orders ||= Order.find_by(status: 'waiting')
     @orders.total = @orders.order_products.collect { |order_prod| order_prod.valid? ? order_prod.unit_price * order_prod.quantity : 0}.sum unless @orders.nil? 
   end
 
-  def order_status
-    @orders ||= Order.find_by(status: 'waiting')
+  def order_product_exist?
+    order_product = OrderProduct.find_by(order_id: @orders.id, product_id: @product.id)
+    order_product.update(quantity: order_product.quantity + 1) unless order_product.nil?
   end
 
-  def product_already_exist?
-    OrderProduct.find_by(order_id: @orders.id, product_id: @product.id)
+  def redirect_order(format)
+    format.html do
+      redirect_to shopping_carts_path
+    end
+    format.json { render :show, status: :ok, location: @orders }
   end
-
-  def orders_params
-    params.require(:order).permit(:status, :user_id,
-      order_products_attributes: %i[quantity product_id order_id])
-  end
-
+  
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,6 +1,31 @@
 class OrderProduct < ApplicationRecord
   belongs_to :product
   belongs_to :order
-
+  
+  before_save :set_unit_price
+  before_save :set_total
   validates :quantity, presence: true
+
+  def unit_price
+    if persisted?
+      self[:unit_price]
+    else
+      product.price
+    end
+  end
+
+  def total
+    unit_price * quantity
+  end
+
+  private
+
+  def set_unit_price
+    self[:unit_price] = unit_price
+  end
+
+  def set_total
+    self[:total] = unit_price * quantity
+  end
+  
 end
